09c421b07efb6079d5aa91b20612efb66325cedd
Abhishek Gupta <92316753+Abhishekgupta47@users.noreply.github.com>
2023-03-16 00:25:29 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-    $('.fa-bars').click(function(){
-      $(this).toggleClass('fa-times'); 
-      $('.navbar').toggleClass('nav-toggle') 
+$(document).ready(function () {
+    $('.fa-bars').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle')
     })
- $(window).on('scroll load',function(){
-    $('.fa-bars').removeClass('fa-times');
+    $(window).on('scroll load', function () {
+        $('.fa-bars').removeClass('fa-times');
 
-    $('.navbar').removeClass('nav-toggle');
+        $('.navbar').removeClass('nav-toggle');
 
-    if($(window).scroll()>30){
-        $('header').addClass('header');
-    }else{
-        $('header').removeClass('header');
+        if ($(window).scrollTop() > 30) {
+            $('header').addClass('header-active');
+        } else {
+            $('header').removeClass('header-active');
 
-    }
- })
+        }
+    })
 })
