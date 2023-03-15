453242fab43806ce71b82d9a0b4fa03207a70944
Abhishek Gupta <92316753+Abhishekgupta47@users.noreply.github.com>
2023-03-15 23:25:23 +0530

    update

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
       $(this).toggleClass('fa-times'); 
       $('.navbar').toggleClass('nav-toggle') 
     })
+ $(window).on('scroll load',function(){
+    $('.fa-bars').removeClass('fa-times');
 
+    $('.navbar').removeClass('nav-toggle');
+
+    if($(window).scroll()>30){
+        $('header').addClass('header');
+    }else{
+        $('header').removeClass('header');
+
+    }
+ })
 })
